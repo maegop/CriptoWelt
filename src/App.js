783f85d67dd9fb34d5d74425b0a8337479999b2f
maegop <megordon@alu.ucam.edu>
2022-01-16 08:12:28 -0500

    footer added to the App.js

--- src/App.js
+++ src/App.js
@@ -30,8 +30,22 @@ const App = () => {
             </Routes>
           </div>
         </Layout>
+
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            Made with Love.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 };
