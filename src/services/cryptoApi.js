09425c37a57284b2ff130e9feaf093c7151d17e3
maegop <megordon@alu.ucam.edu>
2022-01-17 03:05:41 -0500

    global stats in homepage complete

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,40 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "161928393dmshb28f60d4042f54ap1bdb9djsnad5d9862cdeb",
+};
+const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest(`/`),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
+
+/* 
+var options = {
+    method: 'GET',
+    url: 'https://coinranking1.p.rapidapi.com/coins',
+    params: {
+      referenceCurrencyUuid: 'yhjMzLPhuIDl',
+      timePeriod: '24h',
+      tiers: '1',
+      orderBy: 'marketCap',
+      orderDirection: 'desc',
+      limit: '50',
+      offset: '0'
+    },
+    headers: {
+      'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+      'x-rapidapi-key': '161928393dmshb28f60d4042f54ap1bdb9djsnad5d9862cdeb'
+    }
+  }; */
