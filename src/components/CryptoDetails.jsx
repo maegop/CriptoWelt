5c3d3a97e04d5ad944dd4786862cb02a4bb9f8e9
maegop <megordon@alu.ucam.edu>
2022-01-18 14:28:19 -0500

    crypto details added from API and showed belowed

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -159,6 +159,29 @@ const CryptoDetails = () => {
           ))}
         </Col>
       </Col>
+      <Col className="coin-desc-link">
+        <Row className="coin-desc">
+          <Title level={3} className="coin-details-heading">
+            What is {cryptoDetails?.name}
+            {/* {HTMLReactParser(cryptoDetails?.description)} */}
+          </Title>
+          <Col className="coin-links">
+            <Title level={3} className="coin-details-heading">
+              {cryptoDetails?.name} Links
+            </Title>
+            {cryptoDetails?.links.map((link) => (
+              <Row className="coin-link" key={link.name}>
+                <Title level={5} className="link-name">
+                  {link.type}
+                </Title>
+                <a href={link.url} target="_blank" rel="noreferrer">
+                  {link.name}
+                </a>
+              </Row>
+            ))}
+          </Col>
+        </Row>
+      </Col>
     </Col>
   );
 };
